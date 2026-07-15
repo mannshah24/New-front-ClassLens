d5a63e29225086e13d19565b9807b05c289156b0
Mann Shah <mannshah2405@gmail.com>
2026-07-15 11:37:23 +0530

    chore: resolve local conflicts, ignore android/cacerts, and update dependencies/configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,22 @@
+import java.util.Properties
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.reader(Charsets.UTF_8).use { reader ->
+        localProperties.load(reader)
+    }
+}
+
+val trustStorePath = localProperties.getProperty("javax.net.ssl.trustStore")
+if (trustStorePath != null) {
+    System.setProperty("javax.net.ssl.trustStore", trustStorePath)
+}
+val trustStorePassword = localProperties.getProperty("javax.net.ssl.trustStorePassword")
+if (trustStorePassword != null) {
+    System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword)
+}
+
 allprojects {
     repositories {
         google()

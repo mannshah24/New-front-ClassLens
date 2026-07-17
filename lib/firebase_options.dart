c241f5cb3090cd20dd2aaa6d62278aafd62eb236
Krisha Gandhi <krishagandhi0711@gmail.com>
2026-07-17 18:48:09 +0530

    security: remove hardcoded Firebase API key from firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // Firebase support is disabled for the desktop build so the Windows app can
 // link and run without the native Firebase C++ SDK.
@@ -20,24 +21,24 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCAOk5xfJEL7-CzkEmSO4vHGnm1gZN7124',
+  static FirebaseOptions get web => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
     appId: '1:475989075398:web:c8c729e587fe0e12b51af1',
     messagingSenderId: '475989075398',
     projectId: 'claens-5a3e5',
     authDomain: 'claens-5a3e5.firebaseapp.com',
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCAOk5xfJEL7-CzkEmSO4vHGnm1gZN7124',
+  static FirebaseOptions get android => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
     appId: '1:475989075398:android:6bee282c535238afffbec7',
     messagingSenderId: '475989075398',
     projectId: 'claens-5a3e5',
     storageBucket: 'claens-5a3e5.firebasestorage.app',
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCAOk5xfJEL7-CzkEmSO4vHGnm1gZN7124',
+  static FirebaseOptions get ios => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
     appId: '1:475989075398:ios:7d56ef152162c8fdb51af1',
     messagingSenderId: '475989075398',
     projectId: 'claens-5a3e5',
